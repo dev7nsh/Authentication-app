05337478642a4d7755f3c3930a6d475365f11a3c
devansh <dev7nsh@gmail.com>
2025-07-16 15:09:09 +0530

    logitbfunction

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -70,3 +70,18 @@ export const login = async(req,res) => {
         
     }
     }
+    export const logout = (req, res) => {
+        // If using sessions:
+        if (req.session) {
+            req.session.destroy(err => {
+                if (err) {
+                    return res.status(500).json({ message: "Logout failed" });
+                }
+                res.clearCookie('connect.sid');
+                return res.status(200).json({ message: "Logged out successfully" });
+            });
+        } else {
+            // If using JWT or stateless auth:
+            res.status(200).json({ message: "Logged out successfully" });
+        }
+    };
